7b0dd8039f2895e0551de2d9e5a39ba7e3e4460d
Abdelrahman Ashraf <bodeashrafahmed@gmail.com>
2025-12-02 09:18:51 +0200

    start

--- Corn/dailyMembershipCheck.js
+++
@@ -1,7 +0,0 @@
-const cron = require("node-cron");
-const MembershipService = require("../Services/Services");
-
-cron.schedule("0 0 * * *", async () => {
-    console.log("⏳ Running daily membership check...");
-    await MembershipService.decreaseDaily();
-});
