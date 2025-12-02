2f965cbd779c4fc42d2d1d78d1f15622c0d1fd1f
Abdelrahman Ashraf <bodeashrafahmed@gmail.com>
2025-12-02 12:14:30 +0200

    init commit

--- server.ts
+++ server.ts
@@ -2,12 +2,18 @@ import "dotenv/config";
 import express from "express";
 import "./Config/DataBase.js";
 import TrainerProfileRoutes from "./Routes/TrainerProfileRoutes.ts";
+import cors from "cors";
 
 // Initialize Express
 const app = express();
 
 // Middleware
 app.use(express.json());
+app.use(cors({
+    origin: "*",
+    methods: ["GET","POST","PUT","DELETE","PATCH"],
+    credentials: true
+}));
 
 // Routes
 app.use("/api/v1/trainer-profile", TrainerProfileRoutes);
