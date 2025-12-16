6a4baed7ce03a4fd423e96ce007d7ce87f7067f6
Abdelrahman Ashraf <bodeashrafahmed@gmail.com>
2025-12-16 02:52:37 +0200

    init commit

--- Models/SpecializationModel.ts
+++ Models/SpecializationModel.ts
@@ -21,4 +21,4 @@ const SpecializationModel = new mongoose.Schema({
 
 })
 
-export default mongoose.model('Specialization', SpecializationModel)
+export default mongoose.model('specialization', SpecializationModel)
