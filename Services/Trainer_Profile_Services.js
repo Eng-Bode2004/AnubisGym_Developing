f654e7af38c7dbd613ba61abd8062fb249681c30
Abdelrahman Ashraf <bodeashrafahmed@gmail.com>
2025-12-16 02:27:53 +0200

    init commit

--- Services/Trainer_Profile_Services.js
+++ Services/Trainer_Profile_Services.js
@@ -151,7 +151,6 @@ class Trainer_Profile_Services {
             Gender: traineeGender,
             isVerified: true
         })
-            .populate("specialization");
 
         return {
             trainee_gender: traineeGender,
